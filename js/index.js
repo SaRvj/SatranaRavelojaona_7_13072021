0ab20c6a246301b255bafce70b805fff4985050e
Satrana Ravelojoana <satranaravelojaona@gmail.com>
2021-12-10 16:03:23 +0100

    ajustement indentations

--- js/index.js
+++ js/index.js
@@ -7,13 +7,13 @@ import { Recipes } from "./Recipes.js";
 let recipes;
 
 export function loadScripts() {
-  recipes = new Recipes(recipesArr);
-  recipes.filterTagsResultList();
-  recipes.diplayRecipes(recipes.filteredRecipes);
-  listOfInputs.forEach((input) => {
-    input.userSearchEventListener(recipes);
-    input.openListOfTagsHandler(recipes);
-  });
+    recipes = new Recipes(recipesArr);
+    recipes.filterTagsResultList();
+    recipes.diplayRecipes(recipes.filteredRecipes);
+    listOfInputs.forEach((input) => {
+        input.userSearchEventListener(recipes);
+        input.openListOfTagsHandler(recipes);
+    });
 }
 
 subject.subscribe(loadScripts);
