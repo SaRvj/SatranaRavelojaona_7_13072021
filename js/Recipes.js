e213bb2babc30c159cb8e46ed78e3ab01d77d994
Satrana Ravelojoana <satranaravelojaona@gmail.com>
2021-12-13 10:50:23 +0100

    fonction recherche principal et filtre par tag

--- js/Recipes.js
+++ js/Recipes.js
@@ -20,4 +20,69 @@ export class Recipes {
         this.mainSearchContent;
         this.selectedTags = { ingredients: [], appliances: [], ustensils: [] };
     }
+
+    mainSearch(userSearch) {
+        const formattedRecipes = new FormattedRecipes(this.recipesList);
+        let filteredIdRecipes = [];
+        for (let i in formattedRecipes.ingredientsFormatted) {
+            if (formattedRecipes.ingredientsFormatted[i].includes(userSearch) || formattedRecipes.nameFormatted[i].includes(userSearch) || formattedRecipes.descriptionFormatted[i].includes(userSearch)) {
+                filteredIdRecipes.push(formattedRecipes.id[i]);
+            }
+        }
+        this.filteredRecipes = this.recipesList.filter((recipe) => {
+            return filteredIdRecipes.includes(recipe.id);
+        });
+    
+        this.diplayRecipes(this.filteredRecipes);
+    }
+
+    filterTagsResultList(currentInput, userSearch) {
+        this.filteredIngredientsTags = [];
+        this.filteredApplianceTags = [];
+        this.filteredUstensilsTags = [];
+        if (currentInput != undefined) this.currentInput = currentInput;
+        if (this.currentInput == "ingredients") {
+            this.filteredRecipes.map((recipes) => {
+                recipes.ingredients.map((ingredient) => {
+                    if (userSearch != undefined) {
+                        if (!this.filteredIngredientsTags.includes(ingredient.ingredient.toLowerCase()) && ingredient.ingredient.toLowerCase().includes(userSearch)) {
+                            this.filteredIngredientsTags.push(ingredient.ingredient.toLowerCase());
+                        }
+                    } else {
+                        if (!this.filteredIngredientsTags.includes(ingredient.ingredient.toLowerCase())) {
+                            this.filteredIngredientsTags.push(ingredient.ingredient.toLowerCase());
+                        }
+                    }
+                });
+            });
+        }
+        if (this.currentInput == "appliance") {
+            this.filteredRecipes.map((recipes) => {
+                if (userSearch != undefined) {
+                    if (!this.filteredApplianceTags.includes(recipes.appliance.toLowerCase()) && recipes.appliance.toLowerCase().includes(userSearch)) {
+                        this.filteredApplianceTags.push(recipes.appliance.toLowerCase());
+                    }
+                } else {
+                    if (!this.filteredApplianceTags.includes(recipes.appliance.toLowerCase())) {
+                        this.filteredApplianceTags.push(recipes.appliance.toLowerCase());
+                    }
+                }
+            });
+        }
+        if (this.currentInput == "ustensils") {
+            this.filteredRecipes.map((recipes) => {
+                recipes.ustensils.map((ustensil) => {
+                    if (userSearch != undefined) {
+                        if (!this.filteredUstensilsTags.includes(ustensil.toLowerCase()) && ustensil.toLowerCase().includes(userSearch)) {
+                            this.filteredUstensilsTags.push(ustensil.toLowerCase());
+                        }
+                    } else {
+                        if (!this.filteredUstensilsTags.includes(ustensil.toLowerCase())) {
+                            this.filteredUstensilsTags.push(ustensil.toLowerCase());
+                        }
+                    }
+                });
+            });
+        }
+    }
 }
