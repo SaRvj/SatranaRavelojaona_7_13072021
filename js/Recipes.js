911a43f4e635fb57f131c8cdb4b117d4b676654a
Satrana Ravelojoana <satranaravelojaona@gmail.com>
2021-12-13 09:55:05 +0100

    création de class formattedrecipes

---
+++ js/Recipes.js
@@ -0,0 +1,10 @@
+import { capitalizeFirstLetter } from "./functions.js";
+
+class FormattedRecipes {
+    constructor(recipes) {
+        this.id = recipes.map((recipes) => recipes.id);
+        this.ingredientsFormatted = recipes.map((recipes) => recipes.ingredients.map((ingredient) => ingredient.ingredient.toLowerCase()));
+        this.nameFormatted = recipes.map((recipes) => recipes.name.toLowerCase());
+        this.descriptionFormatted = recipes.map((recipes) => recipes.description.toLowerCase());
+    }
+}
