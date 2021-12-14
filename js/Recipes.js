667653d5aad6394c15f29da5dcfee4f1ef5b674d
Satrana Ravelojoana <satranaravelojaona@gmail.com>
2021-12-14 10:15:35 +0100

    rajout code effacé

--- js/Recipes.js
+++ js/Recipes.js
@@ -85,6 +85,232 @@ export class Recipes {
             });
         }
     }
-    
+
+    //afficher les recettes
+    diplayRecipes(filteredRecipes) {
+        let recipesCards = document.querySelector("#recipes");
+        recipesCards.innerHTML = "";
+        recipesCards.innerHTML += `
+            ${filteredRecipes
+                .map(
+                    (recipe) => 
+                    `
+                        <div class="col-12 col-md-6 col-xl-4">
+                            <div class="card">
+                                <div class="card__picture"></div>
+                                <div class="card__informations pt-4 p-3">
+                                    <div class="d-flex justify-content-between card__header mb-4">
+                                        <div class="card__title">${recipe.name}</div>
+                                        <div class="card__duration">
+                                            <img class="logo" src="./images/timer.svg" alt="Recipe duration" />
+                                            ${recipe.time} min
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between card__description">
+                                        <ul class="card__ingredients">
+                                            ${recipe.ingredients
+                                                .map(
+                                                    (ingredient) => 
+                                                    `
+                                                        <li class="d-flex flex-wrap">
+                                                            <p class="card__ingredients__name "> ${
+                                                                ingredient.ingredient
+                                                            } </p>  
+                                                            <div class="d-flex ">
+                                                                ${
+                                                                    ingredient.quantity
+                                                                    ? `<p class="card__ingredients__quantity"> : ${ingredient.quantity} </p>`
+                                                                    : ""
+                                                                }
+                                                                ${
+                                                                    ingredient.unit
+                                                                    ? ` <p class="card__ingredients__unit"> ${ingredient.unit}  </p>`
+                                                                    : ""
+                                                                } 
+                                                            </div>                  
+                                                        </li>
+                                                    `
+                                                            
+                                                )
+                                            .join("")}
+                                        </ul>
+                                        <div class="card__instructions">
+                                            <p class="card__instructions_text">
+                                                ${recipe.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `
+                )
+                .join("")}`;
+                if (filteredRecipes.length == 0) {
+                    recipesCards.innerHTML += `
+                    <div class="col-12 mt-5 card__empty"> Aucune recette ne correspond à votre critère... vous pouvez chercher "tarte aux pommes", "poisson", etc.</div>
+                    `;
+                }
+    }
+
+    displayListOfTags(tagResultsList) {
+        let resultListUl = tagResultsList;
+        let tagsToHandle;
+
+        if (this.currentInput == "ingredients")
+            tagsToHandle = this.filteredIngredientsTags;
+        if (this.currentInput == "appliance")
+            tagsToHandle = this.filteredApplianceTags;
+        if (this.currentInput == "ustensils")
+          tagsToHandle = this.filteredUstensilsTags;
+        resultListUl.innerHTML = "";
+        tagsToHandle.forEach(
+            (tag) =>
+            (resultListUl.innerHTML += `
+            <li> ${capitalizeFirstLetter(tag)}</li>
+            `)
+        );
+    }
+
+    clickOnTagsHandler(tagResultsList, userSearch) {
+        tagResultsList.querySelectorAll("li").forEach((tagToAdd) => {
+            tagToAdd.addEventListener("click", (e) => {
+                if (
+                    this.currentInput == "ingredients" &&
+                    !this.selectedTags.ingredients.includes(
+                        e.target.textContent.toLowerCase().trim()
+                    )
+                ) {
+                    this.selectedTags.ingredients.push(
+                        e.target.textContent.toLowerCase().trim()
+                    );
+                    this.filterRecipesAfterTagSelection(tagResultsList, userSearch);
+                }
+                if (
+                    this.currentInput == "appliance" &&
+                    !this.selectedTags.appliances.includes(
+                      e.target.textContent.toLowerCase().trim()
+                    )
+                ) {
+                    this.selectedTags.appliances.push(
+                        e.target.textContent.toLowerCase().trim()
+                    );
+                    this.filterRecipesAfterTagSelection(tagResultsList, userSearch);
+                }
+                if (
+                    this.currentInput == "ustensils" &&
+                    !this.selectedTags.ustensils.includes(
+                        e.target.textContent.toLowerCase().trim()
+                    )
+                ) {
+                    this.selectedTags.ustensils.push(
+                        e.target.textContent.toLowerCase().trim()
+                    );
+                    this.filterRecipesAfterTagSelection(tagResultsList, userSearch);
+                }
+                this.displayOrRemoveSelectedTags(tagResultsList);
+            });
+        });
+    }
+
+    filterRecipesAfterTagSelection(tagResultsList, userSearch) {
+        let newArray = [];
+        let arrOfRecipes;
+        if (this.mainSearchContent != undefined) {
+            this.mainSearch(this.mainSearchContent);
+            if (userSearch != undefined) this.mainSearch(userSearch);
+            arrOfRecipes = this.filteredRecipes;
+        } else arrOfRecipes = this.recipesList;
+        arrOfRecipes.filter((recipes) => {
+            const filterRecipesByTags = (arr1, arr2) => {
+                let ingredientsArray = [];
+                let appliancesArray = [];
+                for (let i in arr1.ingredients) {
+                    //"push" des ingredients de chaque recette dans un tableau vide
+                    ingredientsArray.push(
+                        arr1.ingredients.map((ingredient) =>
+                            ingredientsArray.push(ingredient.ingredient.toLowerCase())
+                        )
+                    );
+                }
+                for (let i in arr1.appliance) {
+                    //"push" des appareils de chaque recette dans un tableau vide
+                    appliancesArray.push(arr1.appliance.toLowerCase());
+                }
+                const foundIngredients = arr2.ingredients.every(
+                    (elt) => ingredientsArray.indexOf(elt) >= 0
+                );
+                const foundAppliances = arr2.appliances.every(
+                    (elt) => appliancesArray.indexOf(elt) >= 0
+                );
+                const foundUstensils = arr2.ustensils.every((elt) => {
+                    const lowerCaseUstensils = arr1.ustensils.map((tag) =>
+                        tag.toLowerCase()
+                    );
+                    return lowerCaseUstensils.indexOf(elt.toLowerCase()) >= 0;
+                });
+                if (
+                    foundIngredients &&
+                    foundAppliances &&
+                    foundUstensils &&
+                    !newArray.includes(recipes)
+                ){
+                    newArray.push(recipes);
+                }
+            };
+            filterRecipesByTags(recipes, this.selectedTags);
+        });
+        this.filteredRecipes = newArray;
+        this.diplayRecipes(this.filteredRecipes);
+        this.filterTagsResultList(this.currentInput, userSearch);
+        this.displayListOfTags(tagResultsList);
+        this.clickOnTagsHandler(tagResultsList);
+    }
+
+    displayOrRemoveSelectedTags(tagResultsList) {
+        let selectTagsResultList = document.querySelector(".selected-tags");
+        selectTagsResultList.innerHTML = "";
+        this.selectedTags.ingredients.map((tag) => renderTags(tag, "ingredients"));
+        this.selectedTags.appliances.map((tag) => renderTags(tag, "appliances"));
+        this.selectedTags.ustensils.map((tag) => renderTags(tag, "ustensils"));
+        function renderTags(tag, type) {
+            selectTagsResultList.innerHTML += `<li class="p-3  selected-tags__${type}">${capitalizeFirstLetter(
+                tag
+            )}</li>`;
+        }
+
+        selectTagsResultList.querySelectorAll("li").forEach((tagToRemove) => {
+            tagToRemove.addEventListener("click", () => {
+                let indexOfTagToRemove;
+                if (tagToRemove.classList.contains("selected-tags__ingredients")) {
+                    indexOfTagToRemove = this.selectedTags.ingredients.indexOf(
+                        tagToRemove.textContent.toLocaleLowerCase()
+                    );
+                    this.selectedTags.ingredients.splice(indexOfTagToRemove, 1);
+                }
+                if (tagToRemove.classList.contains("selected-tags__appliances")) {
+                    indexOfTagToRemove = this.selectedTags.appliances.indexOf(
+                      tagToRemove.textContent.toLocaleLowerCase()
+                    );
+                    this.selectedTags.appliances.splice(indexOfTagToRemove, 1);
+                }
+                if (tagToRemove.classList.contains("selected-tags__ustensils")) {
+                    indexOfTagToRemove = this.selectedTags.ustensils.indexOf(
+                      tagToRemove.textContent.toLocaleLowerCase()
+                    );
+                    this.selectedTags.ustensils.splice(indexOfTagToRemove, 1);
+                }
+                if (
+                    this.selectedTags.ingredients.length == 0 &&
+                    this.selectedTags.appliances.length == 0 &&
+                    this.selectedTags.ustensils.length == 0 &&
+                    this.mainSearchContent != undefined
+                )
+                    this.mainSearch(this.mainSearchContent);
+                else this.filterRecipesAfterTagSelection(tagResultsList);
+                this.displayOrRemoveSelectedTags(tagResultsList);
+            });
+        });
+    }
 
 }
