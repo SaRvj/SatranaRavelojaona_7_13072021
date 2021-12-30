0bd75bdf5a489d27426279cbc95281954807f533
Satrana Ravelojoana <satranaravelojaona@gmail.com>
2021-12-30 19:17:29 +0100

    modification recipesjs

--- js/Recipes.js
+++ js/Recipes.js
@@ -16,8 +16,8 @@ export class Recipes {
         this.filteredIngredientsTags = [];
         this.filteredApplianceTags = [];
         this.filteredUstensilsTags = [];
-        this.currentInput;
-        this.mainSearchContent;
+        this.currentInput = "";
+        this.mainSearchContent = "";
         this.selectedTags = { ingredients: [], appliances: [], ustensils: [] };
     }
 
