42b354e875eef63fcb3cc75046dd139a1a5c7a05
Satrana Ravelojoana <satranaravelojaona@gmail.com>
2022-01-07 10:50:08 +0100

    nettoyage code

--- js/Recipes.js
+++ js/Recipes.js
@@ -12,32 +12,6 @@ export class Recipes {
         this.selectedTags = { ingredients: [], appliances: [], ustensils: [] };
     }
 
-    // mainSearch(word) {
-    //     let recipestmp = [];
-    //     for (let i = 0; i < this.recipesList.length; i++) {
-    //         let recipetmp = this.recipesList[i];
-    //         if (
-    //             recipetmp.name.toLowerCase().includes(word.toLowerCase()) ||
-    //             recipetmp.description.toLowerCase().includes(word.toLowerCase())
-    //         ) {
-    //             recipestmp.push(recipetmp);
-    //         } else {
-    //             const ingredientofrecipe = recipetmp.ingredients;
-    //             for (let j = 0; j < ingredientofrecipe.length; j++) {
-    //                 let ingredienttmp = ingredientofrecipe[j];
-    //                 if (
-    //                     ingredienttmp.ingredient.toLowerCase().includes(word.toLowerCase())
-    //                 ) {
-    //                     recipestmp.push(recipetmp);
-    //                     j = ingredientofrecipe.length;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     this.filteredRecipes = recipestmp;
-    //     this.displayRecipes(this.filteredRecipes);
-    // }
-
     mainSearch(word) {
         let recipestmp = [];
         for (let i = 0; i < this.recipesList.length; i++) {
